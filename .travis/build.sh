9aa7390cb9a9c2b1e7d114876215dcfd810e32d8
donacrio <donatien.criaud@gmail.com>
2020-04-30 10:46:07 +0200

    finish travis setup

--- .travis/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -x
-
-cargo build --target $TARGET --verbose
-cargo build --target $TARGET --verbose --release
-mkdir -p target/executable
-cp target/${TARGET}/debug/crawler target/executable/crawler-${TRAVIS_RUST_VERSION}-${TARGET}-debug
-cp target/${TARGET}/release/crawler target/executable/crawler-${TRAVIS_RUST_VERSION}-${TARGET}
