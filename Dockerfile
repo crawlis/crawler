0e8516b5bfeeaa329af64ee305392db410bc5b74
donacrio <donatien.criaud@gmail.com>
2020-05-02 16:36:46 +0200

    first complete crawler

--- Dockerfile
+++ Dockerfile
@@ -1,41 +1,45 @@
-### NOTES ###
-#
 # This is a development dockerfile optimized to :
 #   - Reduce the build time: non-project binaries are cached
 #   - Reduce the image space: the project is installed as a binary runnable from scratch image
-#
 
 ARG RUST_VERSION=stable
 
-### Building the binary
 
+##### Building the binary #####
 FROM clux/muslrust:${RUST_VERSION} as builder
-RUN groupadd -g 10001 -r dockergrp && useradd -r -g dockergrp -u 10001 dockeruser
+
+# RUN groupadd -g 10001 -r dockergrp && useradd -r -g dockergrp -u 10001 dockeruser
+
 # Download the target for static linking.
 RUN rustup target add x86_64-unknown-linux-musl
+
 # Create a new empty shell project
 RUN USER=root cargo new --bin --vcs none crawler
 WORKDIR /volume/crawler
+
 # Copy the manifests
 COPY ./Cargo.lock ./Cargo.lock
 COPY ./Cargo.toml ./Cargo.toml
-# Build step to cache dependencies
-RUN cargo build --release --target x86_64-unknown-linux-musl
-RUN rm src/*.rs && \
-    rm -rf ./target/x86_64-unknown-linux-musl/release/deps/crawler*
-# Copy the project tree
-COPY ./src ./src
-# Install binaries to run on scratch
-RUN cargo build --release --target x86_64-unknown-linux-musl
-RUN mkdir /build-out && \
-    cp ./target/x86_64-unknown-linux-musl/release/crawler /build-out/
 
-### Building the final image
+# Build step to cache dependencies
+RUN cargo build --target x86_64-unknown-linux-musl
+RUN rm src/*.rs && \
+    rm -rf ./target/x86_64-unknown-linux-musl/debug/deps/crawler*
+
+# Install the binary to run on scratch
+COPY ./src ./src
+RUN cargo build --target x86_64-unknown-linux-musl
+RUN mkdir /build-out && \
+    cp ./target/x86_64-unknown-linux-musl/debug/crawler /build-out/
+
+
+##### Building the final image #####
 FROM scratch
+# Adding the binary
 COPY --from=builder /build-out/crawler .
 # Adding SSL certificates
-COPY --from=builder /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/ca-certificates.crt
-ENV SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
-ENV SSL_CERT_DIR=/etc/ssl/certs
-USER 1000
+#COPY --from=builder /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/ca-certificates.crt
+#ENV SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
+#ENV SSL_CERT_DIR=/etc/ssl/certs
+#USER 1000
 CMD ["./crawler"]
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,54 @@
 use select::document::Document;
 use select::predicate::Name;
 use std::collections::HashMap;
+use std::collections::HashSet;
+use std::collections::VecDeque;
 use std::error::Error;
+use url::Url;
 
 const STARTING_DOMAIN: &str = "https://www.lemonde.fr/";
+const N_LOOPS: u16 = 1000;
 
 fn main() {
-    let mut map: HashMap<String, Vec<String>> = HashMap::new();
-    map.entry(String::from(STARTING_DOMAIN))
-        .or_insert(crawl(STARTING_DOMAIN).unwrap_or(Vec::new()));
+    let mut map: HashMap<Url, HashSet<Url>> = HashMap::new();
+    let mut queue: VecDeque<Url> = VecDeque::new();
+    queue.push_back(Url::parse(STARTING_DOMAIN).unwrap());
 
-    for key in map.keys() {
-        let mut map_copy = map.clone();
-        let crawling_urls = map_copy.get(key).unwrap().clone();
-        for url in crawling_urls {
-            map_copy
-                .entry(url.clone())
-                .or_insert(crawl(&url).unwrap_or(Vec::new()));
+    let mut i: u16 = 0;
+    while !queue.is_empty() && i < N_LOOPS {
+        let crawling_url: Url = queue.pop_front().unwrap();
+        if !map.contains_key(&crawling_url) {
+            let new_links: Vec<String> = crawl(crawling_url.clone()).unwrap_or(Vec::new());
+            let new_urls: HashSet<Url> = links_to_urls(crawling_url.clone(), new_links);
+            for new_url in new_urls.iter() {
+                queue.push_back(new_url.clone());
+            }
+            map.insert(crawling_url, new_urls);
         }
+        i += 1;
     }
+    println!("{:#?}", map);
 }
 
-fn crawl(url: &str) -> Result<Vec<String>, Box<dyn Error>> {
+fn crawl(url: Url) -> Result<Vec<String>, Box<dyn Error>> {
     println!("Crawling {}", url);
     let response = reqwest::blocking::get(url)?;
-    let urls = Document::from_read(response)?
+    let links = Document::from_read(response)?
         .find(Name("a"))
         .filter_map(|a| a.attr("href"))
-        .map(|url| String::from(url))
+        .map(|link| String::from(link))
         .collect::<Vec<String>>();
-    Ok(urls)
+    Ok(links)
+}
+
+fn links_to_urls(parent_url: Url, links: Vec<String>) -> HashSet<Url> {
+    let mut urls: HashSet<Url> = HashSet::new();
+    for link in links.iter() {
+        if let Ok(url) = Url::parse(link) {
+            urls.insert(url);
+        } else if let Ok(url) = parent_url.join(link) {
+            urls.insert(url);
+        }
+    }
+    return urls;
 }
