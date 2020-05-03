aefa97637c17c001499eb51cb8fd7a24b766e50b
donacrio <donatien.criaud@gmail.com>
2020-05-03 23:29:01 +0200

    add dotenv support

--- src/crawler.rs
+++ src/crawler.rs
@@ -7,80 +7,107 @@ use std::collections::VecDeque;
 use std::error::Error;
 use url::Url;
 
-#[derive(Deserialize, Serialize)]
-struct CrawlingOutput {
-    parent: String,
-    childs: Vec<String>,
+pub struct CrawlerConfig {
+    keeper_url: Url,
 }
 
-impl CrawlingOutput {
-    fn from(parent: String, childs: Vec<String>) -> CrawlingOutput {
-        CrawlingOutput { parent, childs }
+impl CrawlerConfig {
+    pub fn new(keeper_url: Url) -> CrawlerConfig {
+        CrawlerConfig { keeper_url }
     }
 }
 
-pub fn run(starting_url: &str, n_loops: u16) -> Result<(), Box<dyn Error>> {
-    let keeper_client = reqwest::blocking::Client::new();
-    let mut map: HashMap<Url, HashSet<Url>> = HashMap::new();
-    let mut queue: VecDeque<Url> = VecDeque::new();
-    queue.push_back(Url::parse(starting_url)?);
+pub struct Crawler {
+    client: reqwest::blocking::Client,
+    keeper_url: Url,
+}
 
-    let mut i: u16 = 0;
-    while !queue.is_empty() && i < n_loops {
-        let crawling_url: Url = queue.pop_front().unwrap();
-        if !map.contains_key(&crawling_url) {
-            let new_links: Vec<String> = crawl(&crawling_url).unwrap_or(Vec::new());
-            let new_urls: HashSet<Url> = links_to_urls(&crawling_url, new_links);
-            for new_url in new_urls.iter() {
-                queue.push_back(new_url.clone());
+impl Crawler {
+    pub fn new(config: CrawlerConfig) -> Crawler {
+        Crawler {
+            client: reqwest::blocking::Client::new(),
+            keeper_url: config.keeper_url,
+        }
+    }
+
+    pub fn run(&self, starting_url: Url) -> Result<(), Box<dyn Error>> {
+        let mut map: HashMap<Url, HashSet<Url>> = HashMap::new();
+        let mut queue: VecDeque<Url> = VecDeque::new();
+        queue.push_back(starting_url);
+
+        while !queue.is_empty() {
+            let crawling_url: Url = queue.pop_front().unwrap();
+            if !map.contains_key(&crawling_url) {
+                if let Ok(crawling_results) = Crawler::crawl(crawling_url) {
+                    for child in crawling_results.children.iter() {
+                        queue.push_back(child.clone());
+                    }
+                    map.insert(
+                        crawling_results.parent.clone(),
+                        crawling_results.children.clone(),
+                    );
+                    self.send_to_keeper(CrawlerResultsRequestBody::from(crawling_results))?;
+                }
             }
-            let crawling_output = CrawlingOutput::from(
-                crawling_url.clone().into_string(),
-                new_urls
-                    .clone()
-                    .iter()
-                    .map(|new_url| new_url.clone().into_string())
-                    .collect::<Vec<String>>(),
-            );
-            map.insert(crawling_url, new_urls);
-            send_to_keeper(&keeper_client, crawling_output)?;
         }
-        i += 1;
+        Ok(())
     }
-    println!("{:#?}", map);
-    Ok(())
+
+    fn crawl(url: Url) -> Result<CrawlerResults, Box<dyn Error>> {
+        println!("Crawling {}", url);
+        let mut found_urls: HashSet<Url> = HashSet::new();
+        let response = reqwest::blocking::get(url.clone())?;
+        Document::from_read(response)?
+            .find(Name("a"))
+            .filter_map(|a| a.attr("href"))
+            .for_each(|link| {
+                if let Ok(url) = Url::parse(link) {
+                    found_urls.insert(url);
+                } else if let Ok(url) = url.join(link) {
+                    found_urls.insert(url);
+                }
+            });
+        Ok(CrawlerResults::from(url, found_urls))
+    }
+
+    fn send_to_keeper(
+        &self,
+        body: CrawlerResultsRequestBody,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.client
+            .post(self.keeper_url.clone())
+            .body(serde_json::to_string(&body).unwrap())
+            .send()?;
+        Ok(())
+    }
 }
 
-fn crawl(url: &Url) -> Result<Vec<String>, Box<dyn Error>> {
-    println!("Crawling {}", url);
-    let response = reqwest::blocking::get(url.clone())?;
-    let links = Document::from_read(response)?
-        .find(Name("a"))
-        .filter_map(|a| a.attr("href"))
-        .map(|link| String::from(link))
-        .collect::<Vec<String>>();
-    Ok(links)
+struct CrawlerResults {
+    parent: Url,
+    children: HashSet<Url>,
 }
 
-fn links_to_urls(parent_url: &Url, links: Vec<String>) -> HashSet<Url> {
-    let mut urls: HashSet<Url> = HashSet::new();
-    for link in links.iter() {
-        if let Ok(url) = Url::parse(link) {
-            urls.insert(url);
-        } else if let Ok(url) = parent_url.join(link) {
-            urls.insert(url);
+impl CrawlerResults {
+    fn from(parent: Url, children: HashSet<Url>) -> CrawlerResults {
+        CrawlerResults { parent, children }
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+struct CrawlerResultsRequestBody {
+    parent: String,
+    children: Vec<String>,
+}
+
+impl CrawlerResultsRequestBody {
+    fn from(crawling_results: CrawlerResults) -> CrawlerResultsRequestBody {
+        CrawlerResultsRequestBody {
+            parent: crawling_results.parent.into_string(),
+            children: crawling_results
+                .children
+                .iter()
+                .map(|child| child.clone().into_string())
+                .collect::<Vec<String>>(),
         }
     }
-    return urls;
-}
-
-fn send_to_keeper(
-    keeper_client: &reqwest::blocking::Client,
-    crawling_output: CrawlingOutput,
-) -> Result<(), Box<dyn std::error::Error>> {
-    keeper_client
-        .post("http://0.0.0.0:3030")
-        .body(serde_json::to_string(&crawling_output).unwrap())
-        .send()?;
-    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,36 @@
 mod crawler;
 
-const STARTING_URL: &str = "https://www.lemonde.fr/";
-const N_LOOPS: u16 = 1000;
+use crawler::{Crawler, CrawlerConfig};
+use dotenv::dotenv;
+use std::env;
+use std::error::Error;
+use std::process;
+use url::Url;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    crawler::run(STARTING_URL, N_LOOPS)?;
-    Ok(())
+const STARTING_URL: &str = "https://www.lemonde.fr/";
+
+fn main() {
+    let starting_url: Url = Url::parse(STARTING_URL).expect("Starting url is not valid");
+    dotenv().expect("No .env file!");
+
+    let config = get_config().unwrap_or_else(|err| {
+        eprintln!("Problem parsing env values: {}", err);
+        process::exit(1);
+    });
+    let crawler = Crawler::new(config);
+    crawler.run(starting_url).unwrap_or_else(|err| {
+        eprintln!("Problem running the crawler: {}", err);
+        process::exit(1);
+    });
+}
+
+fn get_config() -> Result<CrawlerConfig, Box<dyn Error>> {
+    let keeper_host = env::var("KEEPER_HOST")?;
+    let keeper_port = env::var("KEEPER_PORT")?;
+
+    let keeper_url = format!("http://{}:{}", keeper_host, keeper_port);
+    let keeper_url = Url::parse(&keeper_url)?;
+
+    let config = CrawlerConfig::new(keeper_url);
+    Ok(config)
 }
