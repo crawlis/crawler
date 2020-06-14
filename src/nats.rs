70afb47c949e31385b20c6789c584b81dafe7df4
Donatien Criaud <30463908+DonaCrio@users.noreply.github.com>
2020-06-14 20:48:24 +0200

    Get urls from planner (#18)
    * crawler gets url from nats
    
    * add blocking loop
    
    * migrate nats crate
    
    * fix docker-compose

--- src/nats.rs
+++ src/nats.rs
@@ -4,18 +4,38 @@ pub struct NatsPublisher {
 }
 
 impl NatsPublisher {
-    pub fn new(uri: &str, subject: &str) -> Result<NatsPublisher, std::io::Error> {
+    pub fn new(uri: &str, subject: &str) -> std::io::Result<NatsPublisher> {
         let conn = nats::connect(uri)?;
         Ok(NatsPublisher {
             conn,
             subject: String::from(subject),
         })
     }
-    pub fn publish(&self, key: &str, message: Vec<u8>) -> Result<(), std::io::Error> {
+    pub fn publish(&self, key: &str, message: Vec<u8>) -> std::io::Result<()> {
         let subject = format!("{}.{}", &self.subject, key);
         self.conn.publish(&subject, message)
     }
-    pub fn close(self) -> Result<(), std::io::Error> {
+    pub fn close(self) {
+        self.conn.close()
+    }
+}
+
+pub struct NatsSubscriber {
+    conn: nats::Connection,
+    sub: nats::Subscription,
+}
+
+impl NatsSubscriber {
+    pub fn new(uri: &str, subject: &str) -> std::io::Result<NatsSubscriber> {
+        let conn = nats::connect(uri)?;
+        let sub = format!("{}.*", subject);
+        let sub = conn.queue_subscribe(&sub, "crawler")?;
+        Ok(NatsSubscriber { conn, sub })
+    }
+    pub fn get_next_message(&self) -> Option<nats::Message> {
+        self.sub.next()
+    }
+    pub fn close(self) {
         self.conn.close()
     }
 }
